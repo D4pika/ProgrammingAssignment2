f02666962dca94a3e6fd305a98817bc96e3e78fe
D4pika <34044572+D4pika@users.noreply.github.com>
2017-12-22 13:41:10 -0500

    Update cachematrix.R
    to cache the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrix = numeric()) {
+        
+        inv <- NULL  #start clearing the inverse matrix
+        
+        #set: function to input a new matrix
+        set <- function(y) {
+                matrix <<- y
+                inv <<- NULL
+        }
+        
+        #checking if the input is a matrix, otherwise shows an error message
+        if(!is.matrix(matrix)){
+                print("Error: the input argument 'matrix' must be a matrix.")
+                return()
+        }
+        
+        #get: returns the matrix stored
+        get <- function(){
+                matrix
+        }
+        
+        #setinv: stores the inverse of the matrix
+        setinv <- function(inverse) {
+                inv <<- inverse
+        }
+        
+        #getinv: returns the cached inverse
+        getinv <- function() {
+                inv
+        }
+        
+        #listing functions created
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrix) {
+        
+        #inv <- the cached value
+        inv <- matrix$getinv()
+        
+        #if a cached value exists return a message saying that and the value cached
+        if(!is.null(inv)) {
+                message("Getting cached information...")
+                return(inv)
+        }
+        
+        #in case there is a NULL value cached, compute the inverse of the matrix
+        x <- matrix$get()
+        inv <- solve(x)
+        matrix$setinv(inv) #caching value
+        inv #show the result
 }
